eba200dfd069dd7130061a8212746fcd7a730cdd
Andre Luis Soares Alves Monteiro <andremonteiro.cb@hotmail.com>
2021-10-21 19:42:43 -0300

    subindo

---
+++ Fundamentos.Azure.ServiceBus/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Fundamentos.Azure.ServiceBus.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Fundamentos.Azure.ServiceBus.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        private readonly string connectionString;
+
+        public ProductsController(IConfiguration config)
+        {
+            this.config = config;
+            connectionString = this.config.GetValue<string>("AzureServiceBus");
+        }
+
+        [HttpPost("queue")]
+        public async Task<IActionResult> PostQueue(Product product)
+        {
+            await SendMessageQueue(product);
+            return Ok(product);
+        }
+
+        private async Task SendMessageQueue(Product product)
+        {
+            string queueName = "notificar";
+            var client = new QueueClient(connectionString, queueName, ReceiveMode.PeekLock);
+            string messageBody = JsonSerializer.Serialize(product);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            await client.SendAsync(message);
+            await client.CloseAsync();
+        }
+
+        [HttpPost("topic")]
+        public async Task<IActionResult> PostTopic(Product product)
+        {
+            await SendMessageToTopic(product);
+            return Ok(product);
+        }
+
+        private async Task SendMessageToTopic(Product product)
+        {
+            var topicName = "stores";
+
+            var client = new TopicClient(connectionString, topicName);
+            string messageBody = JsonSerializer.Serialize(product);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            await client.SendAsync(message);
+            await client.CloseAsync();
+        }
+
+    }
+}
---
+++ Fundamentos.Azure.ServiceBus/Startup.cs
@@ -0,0 +1,53 @@
+using Fundamentos.Azure.ServiceBus.HostedServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Fundamentos.Azure.ServiceBus
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedService<ProductQueueConsumer>();
+            services.AddHostedService<ProductTopicConsumer1>();
+            services.AddHostedService<ProductTopicConsumer2>();
+            services.AddHostedService<ProductTopicConsumer3>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fundamentos.Azure.ServiceBus", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fundamentos.Azure.ServiceBus v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
